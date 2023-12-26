f5edbf689cce35334acf2e4bf2d2630026e239a8
nibin <nbaby@orthofx.com>
2023-12-26 15:54:00 +0530

    boolean and conditionals: continue and break

--- main.go
+++ main.go
@@ -4,6 +4,26 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("hello, ninjas")
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	names := []string{"mario", "luigi", "bower", "yoshi", "peach"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue // go back up to the for loop next iteration
+		}
+
+		if index > 2 {
+			fmt.Println("breaking at position", index)
+			break // breaks out the loop completely
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 
 }
