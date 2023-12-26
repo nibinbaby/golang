8d9dc75a5d7bc080f9b9f764a31eb6bb99c67afb
nibin <nbaby@orthofx.com>
2023-12-26 15:45:21 +0530

    loops in go

--- main.go
+++ main.go
@@ -4,6 +4,26 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("hello, ninjas")
+	for x := 0; x < 5; x++ {
+		fmt.Println("value of x is :", x)
+	}
 
+	names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+
+	// for in loop ; using range keyword
+	for index, value := range names {
+		fmt.Printf("the position of name %v is %v \n", value, index)
+	}
+
+	// if you dont want to print out the index but only the value you can use _
+	for _, value := range names {
+		fmt.Printf("the value is %v \n", value)
+		value = "new string" // if you alter the value in the loop it doesnt alter the original slice 'names'
+	}
+
+	fmt.Println(names)
 }
