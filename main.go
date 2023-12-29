813eb15c439068497038a532b9b6939d54106397
nibin <nbaby@orthofx.com>
2023-12-29 15:43:03 +0530

    pointers

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import "fmt"
 
+func updateName(n *string) {
+	*n = "wedge"
+}
+
 func main() {
 
-	fmt.Println("hello, ninjas")
+	name := "tifa"
 
+	fmt.Println("memoy address of name is : ", &name)
+
+	m := &name
+
+	fmt.Println("memory address in the variable m : ", m)
+	fmt.Println("value at memory address ", m, " is ", *m)
+
+	fmt.Println(name)
+
+	updateName(m)
+
+	fmt.Println(name)
 }
+
+/*
+
+|--name--|---m---|
+| 0x001  | 0x002 |
+|--------|-------|
+| tifa   | p0x001|
+|--------|-------|
+
+*/
